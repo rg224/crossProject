bc3a648da65f9f35d4e906383b47b95f75208106
Ritika Gupta <guptaritika782@gmail.com>
2020-11-15 14:53:06 +0530

    Text Summarization edited

---
+++ OtherPages/TextSummarization/textSumm_script.js
@@ -0,0 +1,22 @@
+var input = document.getElementById('inputfile');
+var output = document.getElementById('output');
+
+function handleFileSelect()
+{  
+    if (input.files[0]) {
+        var file = input.files[0];           
+        var fr=new FileReader(); 
+        fr.onload = function()
+        { 
+            output.appendChild(document.createTextNode(fr.result));
+            output.style.display = "block";
+            console.log(fr.result)
+        } 
+            
+        // fr.readAsDataURL(file); 
+        fr.readAsText(file); 
+    }
+    else {
+        console.log("No file fetched.")
+    }
+}
