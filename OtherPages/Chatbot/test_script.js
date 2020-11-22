deec1640ae7d4eebc5ec33c12230500bdd865c29
Ritika Gupta <guptaritika782@gmail.com>
2020-11-22 13:45:28 +0530

    sentiment analysis front-end modified

--- OtherPages/Chatbot/test_script.js
+++ OtherPages/Chatbot/test_script.js
@@ -1,56 +1,159 @@
-let mic = document.getElementById("mic");
-let chatareamain = document.querySelector('.chatarea-main');
-let chatareaouter = document.querySelector('.chatarea-outer');
-let text_area = document.getElementById('text_area');
-let send = document.getElementById('send');
+// let mic = document.getElementById("mic");
+// let chatareamain = document.querySelector('.chatarea-main');
+// let chatareaouter = document.querySelector('.chatarea-outer');
+// let text_area = document.getElementById('text_area');
+// let send = document.getElementById('send');
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+// const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// const recognition = new SpeechRecognition();
 
-mic.addEventListener("click", function(){
+// mic.addEventListener("click", function(){
 
-	recognition.start();
-	console.log("Activated");
-})
+// 	recognition.start();
+// 	console.log("Activated");
+// })
 
-function showusermsg(usermsg){
-    let output = '';
-    output += `<div class="chatarea-inner user">${usermsg}</div>`;
+// function showusermsg(usermsg){
+//     let output = '';
+//     output += `<div class="chatarea-inner user">${usermsg}</div>`;
 
-    chatareaouter.innerHTML += output;
-    return chatareaouter;
-}
+//     chatareaouter.innerHTML += output;
+//     return chatareaouter;
+// }
 
-function showchatbotmsg(chatbotmsg){
-    let output = '';
-    output += `<div class="chatarea-inner chatbot">${chatbotmsg}</div>`;
-    chatareaouter.innerHTML += output;
-    return chatareaouter;
-}
+// function showchatbotmsg(chatbotmsg){
+//     let output = '';
+//     output += `<div class="chatarea-inner chatbot">${chatbotmsg}</div>`;
+//     chatareaouter.innerHTML += output;
+//     return chatareaouter;
+// }
 
-send.addEventListener("click", function(){
+// send.addEventListener("click", function(){
 
-	if (text_area.value != ''){
-		showusermsg(text_area.value);
-		text_area.value = '';
+// 	if (text_area.value != ''){
+// 		showusermsg(text_area.value);
+// 		text_area.value = '';
+// 	}
+// 	// console.log(text_area.value);
+// })
+
+// function chatbotvoice(message) {
+// 	const speech = new SpeechSynthesisUtterance();
+// 	speech.text = "Hello, I am Cross, how are you, i am good. What about you!"; // for now this message from chatbot is fixed
+// 	window.speechSynthesis.speak(speech);
+// 	showchatbotmsg(speech.text);
+// 	// chatareamain.appendChild(showchatbotmsg(speech.text));
+// }
+
+// recognition.onresult = function(e){
+// 	let resultindex = e.resultIndex;
+// 	let transcript = e.results[resultindex][0].transcript
+// 	// chatareamain.appendChild(showusermsg(transcript));
+// 	showusermsg(transcript);
+// 	chatbotvoice(transcript);
+
+// 	// console.log(transcript);
+// }
+
+$(function () {
+	function get_response() {
+		console.log('here')
+		
+		var value = $('#text_area').val();
+
+		if (value.length > 0) {
+			$('#text_area').val('');
+
+			var html_string = `<div class="chatarea-inner user">${value}</div>`;
+
+			$('.chatarea-outer').append(html_string);
+			console.log(html_string)
+			console.log($('.chatarea-outer'))
+
+			var socket = new WebSocket('ws://127.0.0.1:8765');
+
+			socket.onmessage = function(event) {
+				data = JSON.parse(event.data);
+
+				var html_string = `<div class="chatarea-inner chatbot">${data['response']}</div>`;
+			
+				$('.chatarea-outer').append(html_string);
+				$('#text_area').val('');
+			};
+
+			var data = {'text': value};
+
+			socket.onopen = function(event) {
+				socket.send(JSON.stringify(data));
+			};
+		} else {
+			// $('.no-message').removeClass('hidden');
+			$('#text_area').val('');
+		}
+		console.log('completed')
 	}
-	// console.log(text_area.value);
-})
 
-function chatbotvoice(message) {
-	const speech = new SpeechSynthesisUtterance();
-	speech.text = "Hello, I am Cross, how are you, i am good. What about you!"; // for now this message from chatbot is fixed
-	window.speechSynthesis.speak(speech);
-	showchatbotmsg(speech.text);
-	// chatareamain.appendChild(showchatbotmsg(speech.text));
-}
+	$('#send').click(function(){
+		console.log("send");
+		get_response();
+	});
 
-recognition.onresult = function(e){
-	let resultindex = e.resultIndex;
-	let transcript = e.results[resultindex][0].transcript
-	// chatareamain.appendChild(showusermsg(transcript));
-	showusermsg(transcript);
-	chatbotvoice(transcript);
+	$('#text_area').keypress(function (e) {
+		var key = e.which;
+		if(key == 13)  // the enter key code
+			{
+			$('#send').click();
+			$('#text_area').val('');
+			}
+	});
 
-	// console.log(transcript);
-}
+	function getmic_response(value) {
+		console.log('here')
+
+		if (value.length > 0) {
+
+			var html_string = `<div class="chatarea-inner user">${value}</div>`;
+
+			$('.chatarea-outer').append(html_string);
+			console.log(html_string)
+			console.log($('.chatarea-outer'))
+
+			var socket = new WebSocket('ws://127.0.0.1:8765');
+
+			socket.onmessage = function(event) {
+				data = JSON.parse(event.data);
+
+				var html_string = `<div class="chatarea-inner chatbot">${data['response']}</div>`;
+			
+				$('.chatarea-outer').append(html_string);
+				$('#text_area').val('');
+			};
+
+			var data = {'text': value};
+
+			socket.onopen = function(event) {
+				socket.send(JSON.stringify(data));
+			};
+		} else {
+			// $('.no-message').removeClass('hidden');
+			$('#text_area').val('');
+		}
+		console.log('completed')
+	}
+	
+	$("#mic").click(function(){
+		const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		const recognition = new SpeechRecognition();
+		
+		recognition.start();
+		console.log("Activated");
+
+		recognition.onresult = function(e){
+			console.log("hi")
+			let resultindex = e.resultIndex;
+			let transcript = e.results[resultindex][0].transcript;
+			getmic_response(transcript);
+		// chatbotvoice(transcript);
+		}
+	  });
+});
--- OtherPages/SentimentAnalysis/sentiment_script.js
+++ OtherPages/SentimentAnalysis/sentiment_script.js
@@ -1,95 +1,73 @@
-const analyze_result = document.getElementById('analyze_result');
+const data_div = document.getElementById('data');
 const container = document.getElementById('show_sentiment');
 
-$(function () {
-	function get_response() {
-        // console.log('here');
-        // $('#url').attr('disabled', 'disabled');
+var pattern = new RegExp("^((http|https|ftp)\://)*([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&%\$#\=~_\-]+))*$");
 
-        container.style.display = "block";
-        
+$(function () {
+    function get_response() {
+        console.log('here');
         var value = $('#text').val();
 
-        if (value == "good") {
-            var message = "POSITIVE";
-            var html_string = `<div id="result" style="background-color: #26a69a;">${message}</div>`;
-            container.innerHTML = html_string;
-            // console.log("data");
+        if (value.length > 0) {
+
+            if(pattern.test(value)) {
+                container.style.display = "block";
+		        var html_string = `<div id="analyze_result">
+                                    <div id="title">
+                                    Title : boAt Rugged v3 Extra Tough Unbreakable Braided Micro: Amazon.in: Computers & Accessories
+                                    </div>
+                            
+                                    <div id="title">
+                                    Reviews Analyzed: 8
+                                    </div>
+                                    
+                                    <div class="row" id="scrape_data">
+                                    <div class="col" id="positive_review" style="text-align: center;">
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                        <div>Positive Reviews</div>
+                                    </div>      
+                                    </div>
+                            
+                                    <div id="title">
+                                    Total Positive Reviews: 7 :: Total Negative Reviews: 1
+                                    </div>
+                            
+                                    <div id="customer_sat">
+                                        Customer Satisfaction via Reviews is 25%
+                                    </div>
+                                </div>`
+                    
+                container.innerHTML = html_string;
+            }
+            else {
+                var message = "POSITIVE";
+                var html_string = `<div id="result" style="background-color: #26a69a;">${message}</div>`;
+                container.innerHTML = html_string;
+            }
         }
         else {
-            var message = "NEGATIVE";
-            var html_string = `<div id="result" style="background-color: red;">${message}</div>`;
-            container.innerHTML = html_string;
-            // console.log("data");
+            alert("Fill the input box");
         }
     }
 
-	$('#analyze').click(function(){
-		get_response();
+    $('#go').click(function(){
+        get_response();
     }); 
     
     $('#text').keypress(function (e) {
-		var key = e.which;
-		if(key == 13)  // the enter key code
+        var key = e.which;
+        if(key == 13)  // the enter key code
         {
-            $('#analyze').click();
+            $('#go').click();
         }
     }); 
-    
-    function scrape_response() {
-        scrape_data.style.display = "block";
-
-    }
-
-    $('#scrape').click(function(){
-        // $('#text').attr('disabled', 'disabled');
-        
-        container.style.display = "block";
-		var html_string = `<div id="analyze_result">
-                            <div id="title">
-                            boAt Rugged v3 Extra Tough Unbreakable Braided Micro: Amazon.in: Computers & Accessories
-                            </div>
-                            <div class="row" id="scrape_data">
-                            <div class="col s6" id="positive_review">
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                                <div>Positive Reviews</div>
-                            </div>      
-                            <div class="col s6 right" id="negative_review">
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                                <div>Negative Reviews</div>
-                            </div>
-                            </div>
-                            <div id="customer_sat">
-                            Customer Satisfaction via Reviews is 25%
-                            </div>
-                        </div>`
-        container.innerHTML = html_string;
-    }); 
-    
-    $('#url').keypress(function (e) {
-		var key = e.which;
-		if(key == 13)  // the enter key code
-        {
-            $('#scrape').click();
-        }
-    }); 
-    
 });
-
 
